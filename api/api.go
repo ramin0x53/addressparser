eceae880f3669b33996121952bfae61d29ae9293
ramin <ramin0x53@gmail.com>
2022-06-03 05:28:35 -0400

    Add logs file

--- api/api.go
+++ api/api.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/ramin0x53/addressparser/parse"
 )
@@ -11,12 +13,23 @@ type request struct {
 
 func handleReq(c *gin.Context) {
 	var req request
-	c.BindJSON(&req)
+
+	err := c.BindJSON(&req)
+	if err != nil {
+		log.Println(err)
+	}
+
 	c.JSON(200, parse.AddrParse(req.Address))
 }
 
 func Run() {
 	router := gin.Default()
+
 	router.POST("/", handleReq)
-	router.Run()
+
+	err := router.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
 }
--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/ramin0x53/addressparser/api"
 )
 
 func main() {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.SetOutput(file)
+
 	api.Run()
 }
